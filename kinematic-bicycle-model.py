c9dc7c42cbbfe5362798032d12b61bf8298e2215
Clinton Enwerem <51852006+coenwerem@users.noreply.github.com>
2021-07-10 14:25:32 +0100

    Added code for circle trajectory generation

--- kinematic-bicycle-model.py
+++ kinematic-bicycle-model.py
@@ -1,4 +1,3 @@
-from _typeshed import StrOrBytesPath
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -29,7 +28,7 @@ class Bicycle():
 class Bicycle(Bicycle):
     def step(self, v, w):
         # ==================================
-        # Kinematic Bicycle model implementation (with numerical intwegration to propagate the kinematics through time)
+        # Kinematic Bicycle model implementation (with numerical integration to propagate the kinematics through time)
         # Implementation below is for every time step
         # ==================================
         self.delta = self.delta +  (w*self.sample_time)
@@ -41,3 +40,34 @@ class Bicycle(Bicycle):
         self.beta = np.arctan((self.lr*np.tan(self.delta))/self.L)
          
         pass
+
+sample_time = 0.01
+time_end = 20
+model = Bicycle()
+##solution_model = BicycleSolution()
+# set delta directly
+model.delta = np.arctan(2/10)
+##solution_model.delta = np.arctan(2/10)
+t_data = np.arange(0,time_end,sample_time)
+x_data = np.zeros_like(t_data)
+y_data = np.zeros_like(t_data)
+
+##x_solution = np.zeros_like(t_data)
+##y_solution = np.zeros_like(t_data)
+
+for i in range(t_data.shape[0]):
+    x_data[i] = model.xc
+    y_data[i] = model.yc
+    model.step(np.pi, 0)
+##x_solution[i] = solution_model.xc
+##y_solution[i] = solution_model.yc
+##solution_model.step(np.pi, 0)
+    
+model.beta = 0
+#solution_model.beta=0
+
+plt.axis('equal')
+plt.plot(x_data, y_data,label='Learner Model')
+# plt.plot(x_solution, y_solution,label='Solution Model')
+plt.legend()
+plt.show()
